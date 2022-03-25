03612f958fa36455525676e4f2624fd0423211fc
Léonard Wojcik <leonardw@tutanota.com>
2022-03-25 17:05:17 +0100

    feat: sort bills by by date - Sort bills by date (from earliest to latest). Needs further testing.

--- Billed-app-FR-Front-main/src/containers/Bills.js
+++ Billed-app-FR-Front-main/src/containers/Bills.js
@@ -33,6 +33,16 @@ export default class {
       .bills()
       .list()
       .then(snapshot => {
+        // Sort bills by date (from earliest to latest)
+        // Why exemple bellow is not working ?
+        // snapshot.sort((a,b) => b.date - a.date)
+        snapshot.sort((a, b) => {
+          const dateA = new Date(a.date)
+          const dateB = new Date(b.date)
+          return dateB - dateA
+        });
+
+        // Format date of bills
         const bills = snapshot
           .map(doc => {
             try {
