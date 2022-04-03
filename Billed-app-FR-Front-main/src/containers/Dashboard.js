7696e5e010987d73ccfab0f082be55847649131d
Léonard Wojcik <leonardw@tutanota.com>
2022-04-03 15:49:30 +0200

    fix(Dashboard): selection from different lists - Enable bill selection from different lists. - Fix list expand bug.

--- Billed-app-FR-Front-main/src/containers/Dashboard.js
+++ Billed-app-FR-Front-main/src/containers/Dashboard.js
@@ -86,30 +86,27 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    // Old
-    // if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    // Fix
-    this.counter = 0;
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
+    e.target.classList.toggle("bill-active");
+    if (e.target.classList.contains("bill-active")) {
       // Set style to the bills that are not selected to default
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+        $(`#open-bill${b.id}`).removeClass("bill-active")
       })
-      // Set style to current selected bill
+      // Set style to current selected bill and show It's content in right dashboard container
+      $(`#open-bill${bill.id}`).addClass("bill-active");
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
     } else {
-      // Reset style of right dashboard container
+      // Show big Billed icon in right dashboard container
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
       $('.dashboard-right-container div').html(`
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
@@ -137,22 +134,24 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
+    if (!e.target.classList.contains("list-active")) {
+      // Open bill list
+      $(`#arrow-icon${this.index}`).toggleClass( "list-active" )
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
     } else {
+      // Close bill list
+      $(`#arrow-icon${this.index}`).toggleClass( "list-active" )
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
-      this.counter ++
     }
 
+    // Add click event to opened bills from lists
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).off("click").click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
